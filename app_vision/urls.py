10e6083093ea58c40b2d952bf87074de74d76ba0
ManvithaPonnapati <rp493@cornell.edu>
2017-03-05 05:32:12 -0500

    send

--- app_vision/urls.py
+++ app_vision/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index2/', views.index2, name='index2'),
+    url(r'^index3/', views.index3, name='index3'),
+
+    
 ]
--- app_vision/views.py
+++ app_vision/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'app_vision/index.html', {})
+
+def index2(request):
+    return render(request, 'app_vision/index2.html', {})
+
+def index3(request):
+    return render(request, 'app_vision/index3.html', {})
